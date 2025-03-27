4ac7ae2f4d68c3f51ea2d889a571639857eac245
Nut <vimudakorn.k@gmail.com>
2025-03-28 00:22:31 +0700

    setting next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  output: "standalone",
   /* config options here */
 };
 
